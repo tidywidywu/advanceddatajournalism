a24118e5dfc2333cc8094016fbe3b7de3ac60f64
Titus <tituswu100@gmail.com>
2019-11-03 01:32:01 -0500

    turned url into soup

--- scrapeassgn.py
+++ scrapeassgn.py
@@ -1,2 +1,12 @@
 import requests, mechanize
 from bs4 import BeautifulSoup
+
+url = 'https://www.mshp.dps.missouri.gov/HP68/SearchAction?searchDate=10/31/2017'
+
+br = mechanize.Browser()
+br.open(url)
+html = br.response().read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+#it runs and works in command line
