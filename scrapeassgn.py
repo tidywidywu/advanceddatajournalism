c635b9450905eb2e5e641d706a263d7270836a58
Titus <tituswu100@gmail.com>
2019-11-04 14:45:50 -0600

    finished scrape assgn

--- scrapeassgn.py
+++ scrapeassgn.py
@@ -1,5 +1,9 @@
 import requests, mechanize
 from bs4 import BeautifulSoup
+import csv
+
+csvfile = open('scrapeassgn.csv','w', newline='')
+hwy_write = csv.writer(csvfile)
 
 url = 'https://www.mshp.dps.missouri.gov/HP68/SearchAction?searchDate=10/31/2017'
 
@@ -8,5 +12,25 @@ br.open(url)
 html = br.response().read()
 
 soup = BeautifulSoup(html, "html.parser")
+
+main_table = soup.find('table',{'class':'accidentOutput'})
+
+rows = main_table.find_all('tr')
+
+for r in rows:
+	cells = r.find_all('td')
+	data = []
+
+	if len(cells) > 0:
+		for c in cells:
+			data.append(c.text.strip())
+
+		hwy_write.writerow(data)
+
+csvfile.close()
+
+
+
+
 
 #it runs and works in command line
